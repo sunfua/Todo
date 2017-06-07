4dec17afaf90b608d714dc6c2e156d2a777a7e7b
sunfua <sunfua@gmail.com>
2017-06-07 15:19:39 +0700

    navbar plus dropdown

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,9 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  host: {
+    '(document:click)':'closeAll()'
+  }
 })
 export class NavbarComponent implements OnInit {
+  menuIsOpen = false;
+  dropdownIsOpen = false;
 
   constructor() { }
   isIn = false;   // store state
@@ -16,4 +21,22 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
   }
 
+  toggleMenu(e:Event){
+    let bool = this.menuIsOpen;
+    this.menuIsOpen = bool===false?true:false;
+    e.stopPropagation();
+  }
+
+  toggleDropdown(e:Event) {
+    let bool = this.dropdownIsOpen;
+    this.dropdownIsOpen=bool===false?true:false;
+    e.stopPropagation();    
+  }
+
+  closeAll(){
+    setTimeout(()=> {
+      this.dropdownIsOpen = false;
+      this.menuIsOpen = false;
+    },200);
+  }
 }
